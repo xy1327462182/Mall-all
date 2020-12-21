dd3dac61ff5752bec3834da823335eefa4711f34
yang <1327462182@qq.com>
2020-12-21 11:11:08 +0800

    搜索数据的获取

--- home/src/pages/common/search/index.js
+++ home/src/pages/common/search/index.js
@@ -3,13 +3,31 @@ require('./index.less')
 var api = require('api')
 var util = require('util')
 
+var cache = {
+  count: 0,
+  data: {},
+  setData: function(key, val) {
+    this.count++
+    this.data[key] = val
+  },
+  getData: function(key) {
+    return this.data[key]
+  }
+}
+
 var page = {
   init: function() {
     this.$input = $('.search-input')
     //输入防抖定时器
     this.inputTimer = null
     this.bindEvent()
-    
+    this.initInputValue()
+  },
+  initInputValue: function() {
+    var keyword = util.getParamFromUrl('keyword') || ''
+    if (keyword) {
+      this.$input.val(keyword)
+    }
   },
   bindEvent: function() {
     var _this = this
@@ -20,8 +38,30 @@ var page = {
       }
       _this.inputTimer = setTimeout(function() {
         var keyword = $.trim(_this.$input.val())
+        _this.handleSearchData(keyword)
       },300)
     })
+  },
+  //发送搜索请求获取数据
+  handleSearchData: function(keyword) {
+    //从缓存中拿数据
+    var list = cache.getData(keyword)
+    if (!list) {
+      //如果缓存没有再发送请求
+      api.getProductsSearchList({
+        data: {
+          keyword: keyword
+        },
+        success: function(res){
+          list = res
+          //存入缓存中
+          cache.setData('list',res)
+        },
+        error: function(err) {
+          console.log('err::', err);
+        }
+      })
+    }
   }
 }
 
